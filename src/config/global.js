522cc57c2444d3a833165c327f6471dc0b567fd7
Cielo <cielodamarisangulo@gmail.com>
2026-03-31 23:13:50 -0500

    repo

--- src/config/global.js
+++ src/config/global.js
@@ -166,7 +166,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/72340049_CF1_DU.pdf',
       },
       {
         icono: 'fas fa-download',
